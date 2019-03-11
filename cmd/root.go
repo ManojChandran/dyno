f7eff0e11e9c76cf00d3e004f0d4be52e7e2ede7
ManojChandran <manoj.meparmbu@gmail.com>
2019-03-11 03:37:46 -0400

    00002 Commit - Edited root.go and created sub command list

--- cmd/root.go
+++ cmd/root.go
@@ -28,13 +28,12 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "dyno",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Short: "A CLI tool to interact with AWS DynamoDB",
+	Long: `dyno CLI tool will help to interact with AWS DynamoDB,
+For example:
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+dyno CLI tool will help in all CURD operations on the DynamoDB tables.
+It will help in additional db maintenance operations also.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	//	Run: func(cmd *cobra.Command, args []string) { },
